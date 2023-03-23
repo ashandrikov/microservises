5dd9ddee8e1ad124c9d70137bc4b28e50a1274b7
ashandrikov <ashandrikov@gmail.com>
2023-03-23 14:56:39 +0200

    Refactoring: delete dependencies on eureka-server and api-gateway; Add different profiles for local, docker & kubernetes

--- customer/src/main/java/com/amigoscode/customer/CustomerApplication.java
+++ customer/src/main/java/com/amigoscode/customer/CustomerApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication(
         scanBasePackages = {
@@ -14,6 +15,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableFeignClients(
         basePackages = "com.amigoscode.clients"
 )
+@PropertySource("classpath:clients-${spring.profiles.active}.properties")
 public class CustomerApplication {
     public static void main(String[] args) {
         SpringApplication.run(CustomerApplication.class, args);
